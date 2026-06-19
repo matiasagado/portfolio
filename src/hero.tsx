51fee53dfc2305121d8ee7f44241dd0f91784fc4
Matias Agado <112040066+matiasagado@users.noreply.github.com>
2026-06-19 16:13:43 -0700

    feat: hero column 1 styling + Courier Prime font

--- src/hero.tsx
+++ src/hero.tsx
@@ -1,19 +1,34 @@
+import { FaGithubSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
 import styles from './hero.module.css';
 
 export default function Hero() {
     return (
         <section className={styles.section}>
             <div className={styles.column1}>
-                <h1>Matias Agado</h1>
-                <h2>Software Engineer</h2>
-                <p>AI | Systems | Product</p>
-                <p>Building products & systems that create real impact</p>
+                <div className={styles.name}>
+                    <span>MATIAS</span>
+                    <br />
+                    <span>AGADO</span>
+                </div>
                 <div>
-                    <button>Linked In Logo</button>
-                    <button>Github Logo</button>
+                    <p className={styles.role}>SOFTWARE ENGINEER</p>
+                    <p className={styles.tagline}>AI | Systems | Product</p>
+                    <p className={styles.description}>Building reliable software systems<br />
+                        that solve real problems and<br />
+                        create meanignful impact.
+                    </p>
+                </div>
+                <div className={styles.linksWrapper}>
+                    <a className={styles.link}>
+                        <FaGithubSquare />
+                    </a>
+                    <a className={styles.link}>
+                        <FaLinkedin />
+                    </a>
                 </div>
             </div>
-            <div>GRID 2</div>
+            <div className={styles.column2}>GRID 2</div>
         </section>
     );
 }

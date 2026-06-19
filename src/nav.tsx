92edfd1c9819b032e72a74cd0b1fb3a27c9c09a1
Matias Agado <112040066+matiasagado@users.noreply.github.com>
2026-06-19 14:39:57 -0700

    feat: nav bar + styles

---
+++ src/nav.tsx
@@ -0,0 +1,18 @@
+import styles from "./nav.module.css";
+
+export default function Nav() {
+    return (
+        <nav className={styles.nav}>
+            <div className={styles.logoContainer}>
+                <a>MA</a>
+            </div>
+            <div className={styles.linksWrapper}>
+                <a>ABOUT</a>
+                <a>EXERIENCE</a>
+                <a>SOFTWARE</a>
+                <a>HARDWARE</a>
+                <a>CONTACT</a>
+            </div>
+        </nav>
+    );
+}

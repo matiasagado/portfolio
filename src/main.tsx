8a9525aa410d038e9346f5909efc62aac78be78f
Matias Agado <112040066+matiasagado@users.noreply.github.com>
2026-06-21 14:45:41 -0700

    feat: feature to overview section

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Nav from './_components/nav.tsx';
 import Hero from './_components/hero.tsx';
-import Featured from './_components/featured.tsx';
+import Overview from './_components/overview.tsx';
 import Experience from './_components/experience.tsx';
 import Software from './_components/software.tsx';
 import Hardware from './_components/hardware.tsx';
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')!).render(
     <Nav />
     <main>
       <Hero />
-      <Featured />
+      <Overview />
       <Experience />
       <Software />
       <Hardware />

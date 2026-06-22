13cc5181e420508231d19a7dfaa6ae57ad72f056
Matias Agado <112040066+matiasagado@users.noreply.github.com>
2026-06-21 20:26:46 -0700

    feat: hero section

---
+++ src/_components/hero.tsx
@@ -0,0 +1,34 @@
+import styles from './styles/hero.module.css';
+import { SiGithub } from "react-icons/si";
+import { GrLinkedinOption } from "react-icons/gr";
+
+export default function Hero() {
+    return (
+        <section className={styles.hero}>
+            <div className={styles.inner}>
+                <div className={styles.left}>
+                    <div className={styles.text}>
+                        <h1 className={styles.role}>SOFTWARE<br />ENGINEER</h1>
+                        <span className={styles.tags}>AI | SYSTEMS | INFRASTRUCTURE</span>
+                        <span className={styles.name}>Matias Agado</span>
+                        <p className={styles.bio}>Building reliable software systems that <br/>
+                        solve real problems and create<br />
+                        meaningful impact.
+                    </p>
+                    </div>
+                    <div className={styles.socials}>
+                        <a target="_blank" href="https://github.com/matiasagadu" className={styles.socialBtn}>
+                            <SiGithub />
+                        </a>
+                        <a target="_blank" href="https://www.linkedin.com/in/matiasagado/" className={styles.socialBtn}>
+                            <GrLinkedinOption />
+                        </a>
+                    </div>
+                </div>
+                <div className={styles.right}>
+                    <img src="/hero.png" alt="Hero" className={styles.heroImg} />
+                </div>
+            </div>
+        </section>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Nav from './_components/nav';
+import Hero from './_components/hero';
 import Contact from './_components/contact';
 import "./index.css";
 
@@ -8,6 +9,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Nav />
     <main>
+      <Hero />
     </main>
     <Contact />
   </StrictMode>,
